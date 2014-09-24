10b649c8c83443bf1ebabadc2daae659207ec4e8
Emilio Wuerges <wuerges@gmail.com>
2014-09-24 13:06:38 -0300

    push to github

--- src/main/scala/VGA.scala
+++ src/main/scala/VGA.scala
@@ -1,40 +1,61 @@
-package VGA
+package main.scala
 
 import Chisel._
 
+class Counter(n: Int) extends Module {
+  val io = new Bundle {
+    val en = Bool(INPUT)
+    val count = UInt(OUTPUT, log2Up(n))
+  }
+
+  val max = UInt(n, log2Up(n))
+
+  val c = Reg(UInt(0, log2Up(n)))
+
+  when (io.en) {
+    when (c === max) {
+      c := UInt(0)
+    }.otherwise {
+      c := c + UInt(1)
+    }
+  }
+}
+
 class VGA(width: Int)  extends Module {
   val io = new Bundle {
-    val r = new UInt(OUTPUT, width)
-    val g = new UInt(OUTPUT, width)
-    val b = new UInt(OUTPUT, width)
-    val h_sync = new Bool(OUTPUT)
-    val v_sync = new Bool(OUTPUT)
+    val r = UInt(OUTPUT, width)
+    val g = UInt(OUTPUT, width)
+    val b = UInt(OUTPUT, width)
+    val h_sync = Bool(OUTPUT)
+    val v_sync = Bool(OUTPUT)
   }
 
   val seg = 50000000
   val frame = seg / 60
-  val line = seg / 600
+  val line = frame / 600
 
-  val counter_frame = Counter(frame)
-  val counter_line = Counter(frame)
+  print(seg, frame, line)
 
-  counter_frame.io.en := true
-  counter_line.io.en := true
+  val counter_frame = Module(new Counter(frame))
+  val counter_line = Module(new Counter(line))
 
-  r := UInt(0)
-  g := UInt(0)
-  b := UInt(0)
+  counter_frame.io.en := Bool(true)
+  counter_line.io.en := Bool(true)
 
-  when(counter_line > 0 && counter_line < 30) {
-    v_sync := Bool(true)
+  io.r := UInt(0)
+  io.g := UInt(0)
+  io.b := UInt(0)
+
+  when(counter_line.io.count > UInt(0) && counter_line.io.count < UInt(30)) {
+    io.v_sync := Bool(true)
   }.otherwise {
-    v_sync := Bool(false)
+    io.v_sync := Bool(false)
   }
 
-  when(counter_frame > 0 && counter_frame < 30) {
-    v_sync := Bool(true)
+  when(counter_frame.io.count > UInt(0) && counter_frame.io.count < UInt(30)) {
+    io.v_sync := Bool(true)
   }.otherwise {
-    v_sync := Bool(false)
+    io.v_sync := Bool(false)
   }
 }
 
