cd67b460bf22d36475eccaa7157664e6c32b6786
Emilio Wuerges <wuerges@gmail.com>
2014-09-24 13:20:57 -0300

    Added tests

---
+++ src/test/scala/TestVGA.scala
@@ -0,0 +1,20 @@
+package test.scala
+
+/**
+ * @author ew
+ *
+ */
+
+import main.scala._
+import Chisel._
+
+class SimpleTest(c: VGA) extends Tester(c) {
+}
+
+object TestVGA {
+  def main(args: Array[String]): Unit = {
+    chiselMainTest(args, () => Module(new VGA(24))) {
+      c => new SimpleTest(c)
+    }
+  }
+}
